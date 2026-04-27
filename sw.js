44152f40d44f4d37bb41aa145b8e1fddd13cee30
samalkahli <118528882+samalkahli@users.noreply.github.com>
2026-04-27 05:06:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', function(event) {
+    self.skipWaiting(); // تفعيل فوري للتحديثات
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim()); // السيطرة على الصفحة فوراً
+});
+
+self.addEventListener('fetch', function(event) {
+    // خلك محايد: خل المتصفح يجيب البيانات من النت بشكل طبيعي ولا تتدخل
+    event.respondWith(fetch(event.request));
+});
